ae5603881ffa9543c043ca1c36324c28d6988696
Sumi <qkobr94@gmail.com>
2022-07-29 15:57:39 +0900

    feat: calculate accuracy

--- plinko/score.js
+++ plinko/score.js
@@ -8,12 +8,18 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
+  const testSetSize = 10;
+  let numberCorrect = 0;
   //1. split data
-  const [testSet, trainingSet] = splitDataset(outputs, 10);
+  const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
   for (let i = 0; i < testSet.length; i++) {
     const bucket = knn(trainingSet, testSet[i][0]);
     console.log("prection:", bucket, "true value:", testSet[i][3]);
+    if (bucket === testSet[i][3]) {
+      numberCorrect++;
+    }
   }
+  console.log("testSetSize", numberCorrect / testSetSize);
 }
 
 //3. run knn algorithme with traning set of data
